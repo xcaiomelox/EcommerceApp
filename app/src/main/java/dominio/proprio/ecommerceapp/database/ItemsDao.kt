e47655415f90cae66636007f498061ff37ecb502
Caio <caio.melo51@gmail.com>
2023-01-26 12:20:08 -0300

    ⚡️ Implementing Navigation and Item model

---
+++ app/src/main/java/dominio/proprio/ecommerceapp/database/ItemsDao.kt
@@ -0,0 +1,9 @@
+package dominio.proprio.ecommerceapp.database
+
+import dominio.proprio.ecommerceapp.model.Item
+
+object ItemsDao {
+
+    val items: MutableList<Item> = mutableListOf()
+
+}
---
+++ app/src/main/java/dominio/proprio/ecommerceapp/model/Item.kt
@@ -0,0 +1,8 @@
+package dominio.proprio.ecommerceapp.model
+
+class Item(
+    val title: String,
+    val description: String,
+    val price: Double,
+    val image: String
+)
